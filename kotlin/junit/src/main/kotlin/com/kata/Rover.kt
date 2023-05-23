df9f62842768922a4111efcd7d196e1e8b3bc21c
Caroline Chuong <pandoraangora@gmail.com>
2023-05-23 14:06:33 +0200

    refactor move method

--- kotlin/junit/src/main/kotlin/com/kata/Rover.kt
+++ kotlin/junit/src/main/kotlin/com/kata/Rover.kt
@@ -5,28 +5,19 @@ data class Rover(
     val position: Position = Position(0,0)
 ) {
     fun receivedCommand(command: Command): Rover {
-        val position = when (command) {
-            Command.FORWARDS -> moveForwards()
-            Command.BACKWARDS -> moveBackwards()
-        }
-        return Rover(direction, position)
+        return Rover(direction, move(command))
     }
 
-    private fun moveForwards(): Position {
-        return when (direction) {
-            Direction.NORTH -> Position(0, position.y + 1)
-            Direction.SOUTH -> Position(0, position.y - 1)
-            Direction.EAST -> Position(position.x + 1, 0)
-            Direction.WEST -> Position(position.x - 1, 0)
+    private fun move(command: Command): Position {
+        val modifier = when (command) {
+            Command.FORWARDS -> 1
+            Command.BACKWARDS -> -1
         }
-    }
-
-    private fun moveBackwards(): Position {
         return when (direction) {
-            Direction.NORTH -> Position(0, position.y - 1)
-            Direction.SOUTH -> Position(0, position.y + 1)
-            Direction.EAST -> Position(position.x - 1, 0)
-            Direction.WEST -> Position(position.x + 1, 0)
+            Direction.NORTH -> Position(0, (position.y + 1 * modifier))
+            Direction.SOUTH -> Position(0, (position.y - 1 * modifier))
+            Direction.EAST -> Position((position.x + 1 * modifier), 0)
+            Direction.WEST -> Position((position.x - 1 * modifier), 0)
         }
     }
 }
